2b6084b2ee3ba7dcd3cfffe0ea7783702a041c90
Harry Burgess <harryburgessjd@gmail.com>
2021-09-21 11:15:49 +0100

    dev: Add shuffle icon and styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import styles from './App.module.scss';
-import Film from "./Components/Film/Film"
+import styles from "./App.module.scss";
+import Film from "./Components/Film/Film";
 
 const App = () => {
+  const shuffleHandler = () => {
+    console.log("Shuffle");
+  };
   return (
     <div className={styles["App"]}>
       <p>Yeah so I'm writing this screenplay, </p>
       <div className={styles["content-wrapper"]}>
         <p>it's like</p>
         <Film />
-        <p>meets</p>
+        <p>
+          meets
+          <img
+            className={styles["shuffle-icon"]}
+            src="/shuffle.svg"
+            alt="Shuffle icon"
+            onClick={shuffleHandler}
+          />
+        </p>
         <Film />
       </div>
     </div>
   );
-}
+};
 
 export default App;
