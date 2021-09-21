03caaed74a3a8c0e00ed827abc960d659ccd59b0
Harry Burgess <harryburgessjd@gmail.com>
2021-09-21 11:50:53 +0100

    dev: Shuffle button functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
+import React, { useState } from "react";
 import styles from "./App.module.scss";
 import Film from "./Components/Film/Film";
 
 const App = () => {
+  const [randomKey, setRandomKey] = useState(10);
   const shuffleHandler = () => {
     console.log("Shuffle");
+    setRandomKey(Math.floor(Math.random() * 999));
   };
   return (
     <div className={styles["App"]}>
       <p>Yeah so I'm writing this screenplay, </p>
       <div className={styles["content-wrapper"]}>
         <p>it's like</p>
-        <Film />
+        <Film key={randomKey + 1} />
         <p>
           meets
           <img
@@ -20,7 +23,7 @@ const App = () => {
             onClick={shuffleHandler}
           />
         </p>
-        <Film />
+        <Film key={randomKey - 1} />
       </div>
     </div>
   );
--- src/Components/Film/Film.js
+++ src/Components/Film/Film.js
@@ -1,17 +1,20 @@
-import React, {useState} from "react";
-import styles from "./Film.module.scss"
-const jsonFilms = require('../../films.json');
+import React from "react";
+import styles from "./Film.module.scss";
+const jsonFilms = require("../../films.json");
 
 const Film = () => {
-  const [randomFilm, setRandomFilm] = useState(jsonFilms[Math.floor(Math.random() * jsonFilms.length)])
+  const randomFilm = jsonFilms[Math.floor(Math.random() * jsonFilms.length)];
   return (
     <div className={styles["film"]}>
-      <a href={`https://www.imdb.com/title/${randomFilm.imdbid}`} target="_blank">
+      <a
+        href={`https://www.imdb.com/title/${randomFilm.imdbid}`}
+        target="_blank"
+      >
         <h3>{randomFilm.title}</h3>
         <img src={randomFilm.poster} alt={`${randomFilm.title} Poster`} />
       </a>
     </div>
-  )
+  );
 };
 
 export default Film;
